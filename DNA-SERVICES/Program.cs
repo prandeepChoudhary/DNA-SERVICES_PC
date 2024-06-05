4932ede98d12b68012e387841a2433ffc2fcaa0e
prandeepchoudhary <prandeep28@outlook.com>
2024-06-05 22:53:11 +0530

    Add Serilog, Depedency Inversion in program class and Security response header

--- DNA-SERVICES/Program.cs
+++ DNA-SERVICES/Program.cs
@@ -2,11 +2,13 @@
 using DNA_SERVICES.Application.Helper;
 using DNA_SERVICES.Application.Interface;
 using DNA_SERVICES.Application.Services;
+using DNA_SERVICES.Configuration.DependencyInjection;
 using DNA_SERVICES.Configuration.Documentation;
 using DNA_SERVICES.Domain.Interfaces.Domain;
 using DNA_SERVICES.Domain.Interfaces.Repository;
 using DNA_SERVICES.Domain.Services;
 using DNA_SERVICES.Infrastructure.Repository;
+using Serilog;
 
 namespace DNA_SERVICES;
 
@@ -21,6 +23,7 @@ public class Program
         builder.Services.AddControllers();
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
         builder.Services.AddEndpointsApiExplorer();
+        builder.Host.UseSerilog((context, configuration) => configuration.ReadFrom.Configuration(context.Configuration));
 
         builder.Services.AddSwaggerGen(options =>
         {
@@ -46,16 +49,22 @@ public class Program
             }
             options.CustomSchemaIds(type => type.Name.Replace("ViewModel", ""));
         });
-
-
         builder.Services.AddAutoMapper(typeof(AutoMapperProfile).Assembly);
-
-        builder.Services.AddTransient<IFeeScheduleService, FeeScheduleServices>();
-        builder.Services.AddTransient<IFeeScheduleDomainService, FeeScheduleDomainServices>();
-        builder.Services.AddTransient<IFeeScheduleRepository, FeeScheduleRepository>();
+        builder.Services.AddDependencies(builder.Configuration);
 
         var app = builder.Build();
 
+        app.Use(async (Context, next) =>
+        {
+            Context.Response.Headers.Add("Content-Security-Policy", "default-src 'self'; frame-ancestore 'none'; form-action 'self';");
+            Context.Response.Headers.Add("Strict-Transport-Security", "max-age=31536000; includeSubDomains");
+            Context.Response.Headers.Add("X-Frame-Options", "DENY");
+            Context.Response.Headers.Add("X-XSS-Protection", "1");
+            Context.Response.Headers.Add("X-Content-Type-Options", "nosniff");
+            Context.Response.Headers.Add("Cache-Control", "no-cache, no-store, must-revalidate, private");
+            Context.Response.Headers.Add("Pragma", "no-cache");
+            await next();
+        });
         // Configure the HTTP request pipeline.
         if (app.Environment.IsDevelopment())
         {
